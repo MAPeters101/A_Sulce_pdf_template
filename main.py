309c375616ad7117ee1ab712f7cb8d664d9e2381
Mark Peters <mapeters101@earthlink.net>
2024-09-13 19:18:44 -0400

    Add 2 pages

---
+++ main.py
@@ -0,0 +1,21 @@
+from fpdf import FPDF
+
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+
+print(type(pdf))
+
+pdf.add_page()
+
+pdf.set_font(family="Times", style="B", size=12)
+pdf.cell(w=0, h=12, txt="Hello There!", align="L", ln=1, border=1)
+pdf.set_font(family="Times", size=8)
+pdf.cell(w=0, h=12, txt="Hi There!", align="L", ln=1, border=1)
+
+pdf.add_page()
+
+pdf.set_font(family="Times", style="B", size=12)
+pdf.cell(w=0, h=12, txt="Hello There!", align="L", ln=1, border=1)
+pdf.set_font(family="Times", size=8)
+pdf.cell(w=0, h=12, txt="Hi There!", align="L", ln=1, border=1)
+
+pdf.output("output.pdf")
